b4f6dcfe78e71f82dddba334a2f5f543a15a9e18
Jerry Ye <jerry1ye10@gmail.com>
2020-04-19 10:08:19 -0400

    initial commit

---
+++ App1/App1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App1
+//
+//  Created by Jerry Ye on 4/19/20.
+//  Copyright © 2020 Jerry Ye. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
