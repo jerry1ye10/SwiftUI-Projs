7850f30349bdbd4cb4d43dfb8888aebb1cb6dfa3
Jerry Ye <jerry1ye10@gmail.com>
2020-04-20 20:07:09 -0400

    visual fixes

--- App2/App2/ContentView.swift
+++ App2/App2/ContentView.swift
@@ -9,8 +9,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    @State var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var previousGuess = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+        VStack(spacing: 30){
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+        ForEach(0 ..< 3) { number in
+            Button(action: {
+                self.flagTapped(number)
+            }) {
+                Image(self.countries[number])
+                    .renderingMode(.original)
+                    .clipShape(Capsule())
+                    .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                    .shadow(color: .black, radius: 2)
+                
+            }
+        }
+            Text("Current Score: \(score)")
+
+            Spacer()
+        }
+            
+
+        }.alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your guess of \(countries[previousGuess]) was. Your score is \(score)"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
+        }
+        
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score = score + 1
+        } else {
+            scoreTitle = "Wrong"
+            score = score - 1
+        }
+        previousGuess = number
+
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
